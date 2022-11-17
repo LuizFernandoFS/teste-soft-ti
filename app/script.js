d86d0d36c443788fbe16c0d56d4f119dafdbb9c7
LuizFernandoFS <lffs2000@outlook.com>
2022-11-17 19:54:18 -0300

    commit inicial

---
+++ app/script.js
@@ -0,0 +1,41 @@
+var app = angular.module('app', []);
+
+app.controller('appCtrll', ['$scope', function ($scope) {
+
+    $scope.clientes = JSON.parse(localStorage.getItem('arrayDeClientes'));
+
+    $scope.redirecionar = function() {
+        window.location.href = "/index.html";
+    }
+
+    $scope.addCliente = function () {
+
+        $scope.cliente = {
+            tipoPessoa: $scope.cliente.tipoPessoa,
+            cpfOrCnpj: $scope.cliente.cpfOrCnpj,
+            nome: $scope.cliente.nome,
+            nomeFantasia: $scope.cliente.nomeFantasia,
+            cep: $scope.cliente.cep,
+            rua: document.getElementById("rua").value,
+            bairro: document.getElementById("bairro").value,
+            cidade: document.getElementById("cidade").value,
+            telefone: $scope.cliente.telefone,
+            email: $scope.cliente.email
+        }
+
+        $scope.clientes = JSON.parse(localStorage.getItem('arrayDeClientes'));
+
+        if ($scope.clientes == null) {
+            localStorage.setItem('arrayDeClientes', '[]');
+            $scope.clientes = []
+        } 
+
+        $scope.clientes.push($scope.cliente);
+
+        localStorage.setItem("arrayDeClientes", JSON.stringify($scope.clientes));
+        alert("Cliente cadastrado com sucesso!");
+        $scope.redirecionar();
+    }
+}]);
+
+
---
+++ app/viaCEP.js
@@ -0,0 +1,23 @@
+function consultaCEP() {
+    let cep = document.querySelector('#cep').value;
+
+    if (cep.length !== 8) {
+        alert('CEP Inválido!')
+        return
+    }
+
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url).then(function (response) {
+        response.json().then((data) => {
+            let rua = document.getElementById('rua').value = (data.logradouro);
+            let bairro = document.getElementById('bairro').value = (data.bairro);
+            let cidade = document.getElementById('cidade').value = (data.localidade);
+            if(rua === undefined && bairro === undefined && cidade === undefined){
+                document.getElementById('rua').value = '';
+                document.getElementById('bairro').value = '';
+                document.getElementById('cidade').value = '';  
+            }
+        })
+    })
+}
